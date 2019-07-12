6062fa5ff4477d7c6ac2bd60a4862fcfc919d68c
Mouhamed NDOYE <ndoyeahmed2602@gmail.com>
2019-07-12 13:13:31 +0000

    cors

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,37 +1,37 @@
 class Auth {
-    constructor() {
-        this.BaseUrl = 'http://localhost:8080/senebiensimmobilier-1.0-SNAPSHOT/rest';
-      this.BaseUrlprod = 'https://sene-bien-immo-api.herokuapp.com/rest';
-    }
+  constructor() {
+    this.BaseUrl = 'http://localhost:8080/senebiensimmobilier-1.0-SNAPSHOT/rest';
+    this.BaseUrlprod = 'https://sene-bien-immo-api.herokuapp.com/rest';
+  }
 
-    login(userData) {
-        return new Promise((resolve, reject) => {
-          fetch(this.BaseUrlprod + '/user/login', {
-                method: 'POST',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(userData)
-            })
-                .then((response) => response.json())
-                .then((responseJson) => {
-                    resolve(responseJson);
-                })
-                .catch((error) => {
-                    reject(error);
-                });
+  login(userData) {
+    return new Promise((resolve, reject) => {
+      fetch(this.BaseUrlprod + '/user/login', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+      })
+        .then((response) => response.json())
+        .then((responseJson) => {
+          resolve(responseJson);
+        })
+        .catch((error) => {
+          reject(error);
         });
-    }
+    });
+  }
 
-    signOut(cb) {
-        sessionStorage.clear();
-        cb();
-    }
+  signOut(cb) {
+    sessionStorage.clear();
+    cb();
+  }
 
-    isAuthenticated() {
-        return !!sessionStorage.getItem("userData");
-    }
+  isAuthenticated() {
+    return !!sessionStorage.getItem("userData");
+  }
 }
 
 export default new Auth();
