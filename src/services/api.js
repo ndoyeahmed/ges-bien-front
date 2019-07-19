550549af9575b579fd21622965aa3dcfe477c575
Mouhamed NDOYE <ndoyeahmed2602@gmail.com>
2019-07-19 03:11:02 +0000

    base url change to prod

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:8080/senebiensimmobilier-1.0-SNAPSHOT/rest/'
+  baseURL: 'https://sene-bien-immo-api.herokuapp.com/rest/'
 });
 
-// this.BaseUrlprod = 'https://sene-bien-immo-api.herokuapp.com/rest';
+// this.BaseUrlprod = 'https://sene-bien-immo-api.herokuapp.com/rest/';
+// baseURL: 'http://localhost:8080/senebiensimmobilier-1.0-SNAPSHOT/rest/'
